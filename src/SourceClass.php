bb20020c8ce5df751c23593b6512209c24e1a494
SelrahcD <c.desneuf@gmail.com>
2021-09-17 11:33:24 +0200

    Everything ok
    ./vendor/bin/phpunit passes
    
    XDEBUG_MODE=coverage ./vendor/bin/infection --debug --log-verbosity=all
     kills 1 mutant
    
    ./vendor/bin/infection --debug --log-verbosity=all kills 1 mutant

---
+++ src/SourceClass.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class SourceClass
+{
+    public function hello(): string
+    {
+        return 'hello';
+    }
+}
---
+++ tests/SourceClassTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use PHPUnit\Framework\TestCase;
+
+class SourceClassTest extends TestCase
+{
+    public function test_hello()
+    {
+        $sourceClass = new SourceClass();
+        $this->assertSame('hello', $sourceClass->hello());
+    }
+}
