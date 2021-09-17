5018d949dde5408ad5d4ae81dbe92fed9e43cd51
SelrahcD <c.desneuf@gmail.com>
2021-09-17 11:55:46 +0200

    Use QuietReporter during infection - Everything ok
    ./vendor/bin/phpunit passes
    
    XDEBUG_MODE=coverage ./vendor/bin/infection --debug --log-verbosity=all
     kills 4 mutants
    
    ./vendor/bin/infection --debug --log-verbosity=all kills 4 mutants

--- tests/SourceClassTest.php
+++ tests/SourceClassTest.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use ApprovalTests\CombinationApprovals;
+use ApprovalTests\Reporters\QuietReporter;
 use PHPUnit\Framework\TestCase;
 
 class SourceClassTest extends TestCase
@@ -11,10 +12,12 @@ class SourceClassTest extends TestCase
     {
         $firstnames = ['Charles', 'Maks'];
 
+        $reporter = getenv('INFECTION') === "1" ? new QuietReporter() : null;
+
         CombinationApprovals::verifyAllCombinations1(function ($firstname) {
             $sourceClass = new SourceClass();
             return $sourceClass->hello($firstname);
-        }, $firstnames);
+        }, $firstnames, $reporter);
 
     }
 }
